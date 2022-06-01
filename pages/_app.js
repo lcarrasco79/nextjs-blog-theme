07145812f21c9a43edacc9a671ac1657040c9bde
Luis Carrasco <lcarrasco@guidewire.com>
2022-05-31 19:56:40 -0500

    Adding user info

--- pages/_app.js
+++ pages/_app.js
@@ -13,4 +13,10 @@ function MyApp({ Component, pageProps }) {
 
 export default withLDProvider({
   clientSideID: process.env.LAUNCHDARKLY_SDK_CLIENT_SIDE_ID,
+  user: {
+      "key": "example_user2",
+      "name": "Example user 2",
+      "email": "User2@example.com",
+      "country": "MEX"
+  }
 })(MyApp);
